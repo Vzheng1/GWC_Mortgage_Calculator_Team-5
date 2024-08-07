72daea291ed8a7ccb63cf12bf75f3308fdc46db8
Trang Hoang <doradora666hoe@gmail.com>
2024-08-07 11:38:52 -0500

    changes 1

--- MortgageCalculator/app/src/main/java/com/example/mortgagecalculator/MainActivity.kt
+++ MortgageCalculator/app/src/main/java/com/example/mortgagecalculator/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        text = "no",
+        text = "yes",
         modifier = modifier
     )
 }
