86bb39c22579299fef5afccb3688bf0b39ba2e9e
Trang Hoang <doradora666hoe@gmail.com>
2024-08-15 12:35:55 -0500

    changes 1

--- MortgageCalculator/app/src/main/java/com/example/mortgagecalculator/MainActivity2.kt
+++ MortgageCalculator/app/src/main/java/com/example/mortgagecalculator/MainActivity2.kt
@@ -1,10 +1,13 @@
 package com.example.mortgagecalculator
 
 import android.os.Bundle
+import android.content.Intent
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.activity.ComponentActivity
+import com.example.mortgagecalculator.ui.theme.MortgageActivity
+
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,6 +26,13 @@ class MainActivity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val mortgageButton1: Button = findViewById(R.id.mortgageButton1)
+
+        mortgageButton1.setOnClickListener {
+            val intent = Intent(this, MortgageActivity::class.java)
+            intent.putExtra("housePrice", 300000)
+            startActivity(intent)
+        }
 
     }
 }
